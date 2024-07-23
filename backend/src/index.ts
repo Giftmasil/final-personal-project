642d3667359f9ed90c99168b2578021dc505999a
Giftmasil <masilagift@gmail.com>
2024-07-23 20:34:26 +0300

    debugging the upload issues

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,12 +12,7 @@ import keyRouter from './routers/keyRouter'
 dotenv.config()
 
 const app = express()
-app.use(
-  cors({
-    credentials: true,
-    origin: ['http://localhost:5173', 'http://localhost:5174', "http://localhost:3000"],
-  })
-)
+app.use(cors())
 
 
 app.use(express.json())
