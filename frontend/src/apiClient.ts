1beb64e678e2012653d3b8705928757710583f71
Giftmasil <masilagift@gmail.com>
2024-07-23 20:44:03 +0300

    uploading the frontend part

--- frontend/src/apiClient.ts
+++ frontend/src/apiClient.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/',
+  baseURL: 'https://final-personal-project-backend.onrender.com',
   headers: {
     'Content-type': 'application/json',
   },
